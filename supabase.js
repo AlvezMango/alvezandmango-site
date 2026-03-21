e319ec812f8d8424de9d019b59266fcae78c657b
AlvezMango <kemanes@cytanet.com.cy>
2026-03-21 15:01:51 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://jbwlwbawssjimugtayts.supabase.co";
+const SUPABASE_KEY = "sb_publishable_fMXuCisXnwyFqYhBuQrrFg_38HgpDgK";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
